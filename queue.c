32338d33681b01cbc6b185bc7e55069cc7345b8d
djonpietro <jpss.joaopedross@gmail.com>
2024-12-14 17:12:13 -0300

    Correcting a bug in the enqueue and dequeue funtions

--- queue.c
+++ queue.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 void enqueue(Queue *queue, void *data) {
-    list_insert_next(queue, NULL, data);
+    list_insert_next(queue, list_tail(queue), data);
 }
 
 void dequeue(Queue *queue) {
-    list_remove_next(queue, list_tail(queue));
+    list_remove_next(queue, list_head(queue));
 }
